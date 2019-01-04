8d8fb55dbb2e58f7ccc2efc2d503ed67c87b3302
Chancy <chancyleath@hotmail.com>
2019-01-03 21:34:52 -0700

    created NoMatch page, updated App.js with react router routes, created Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ArtistProfile from './pages/ArtistProfile';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import NoMatch from './pages/NoMatch';
+import User from './pages/User';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/artist' component={ArtistProfile} />
+          <Route exact path='/user' component={User} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
     );
-  }
-}
+  };
+};
 
 export default App;
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className='nav-wrapper'>
+                <a href='/' className='brand-logo center'>Tatu</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
