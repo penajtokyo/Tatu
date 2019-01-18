b1968815263df27cddcf9f0862d5ac306b873070
Mitchell Waite <mitchellwaite@Aubreys-MacBook-Pro.local>
2019-01-18 10:28:11 -0700

    created error message for improper login; polished modals and added modal buttons to footer; made home page more dynamic and responsive.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -86,51 +86,60 @@ module.exports = {
     })
       .populate("artistData")
       .then(userData => {
-        console.log("userData", userData);
-        bcrypt.compare(req.body.loginPassword, userData.password, function(
-          err,
-          pMatch
-        ) {
-          if (pMatch === true) {
-            // check to see if they are an artists or not
-            if (userData.type === "customer") {
-              req.session.customer = {
-                _id: userData._id,
-                firstName: userData.firstName,
-                lastName: userData.lastName,
-                email: userData.email,
-                phone: userData.phone,
-                type: userData.type
-              };
-              req.session.customer.loggedIn = true;
-              res.json(req.session.customer);
-            } else {
-              req.session.customer = {
-                _id: userData._id,
-                firstName: userData.firstName,
-                lastName: userData.lastName,
-                email: userData.email,
-                phone: userData.phone,
-                type: userData.type,
-                artistData: {
-                  _id: userData.artistData._id,
-                  specialization: userData.artistData.specialization,
-                  pricing: userData.artistData.pricing,
-                  location: userData.artistData.location,
-                  street: userData.artistData.street,
-                  city: userData.artistData.city,
-                  state: userData.artistData.state,
-                  zip: userData.artistData.zip
-                }
-              };
-              req.session.customer.loggedIn = true;
-              res.json(req.session.customer);
+        console.log("User's info:", userData);
+
+        // ***start of updated code*** if no match in db for email, comes back null
+        if (userData === null) {
+          console.log('user email not found');
+          res.json('invalid');
+        } 
+        //if user data is not null then proceed to check if password matches
+        else {
+          bcrypt.compare(req.body.loginPassword, userData.password, function(err, pMatch) {
+            //if password matches
+            if (pMatch === true) {
+              if (userData.type === "customer") {
+                req.session.customer = {
+                  _id: userData._id,
+                  firstName: userData.firstName,
+                  lastName: userData.lastName,
+                  email: userData.email,
+                  phone: userData.phone,
+                  type: userData.type
+                };
+                req.session.customer.loggedIn = true;
+                res.json(req.session.customer);
+              } else {
+                req.session.customer = {
+                  _id: userData._id,
+                  firstName: userData.firstName,
+                  lastName: userData.lastName,
+                  email: userData.email,
+                  phone: userData.phone,
+                  type: userData.type,
+                  artistData: {
+                    _id: userData.artistData._id,
+                    specialization: userData.artistData.specialization,
+                    pricing: userData.artistData.pricing,
+                    location: userData.artistData.location,
+                    street: userData.artistData.street,
+                    city: userData.artistData.city,
+                    state: userData.artistData.state,
+                    zip: userData.artistData.zip
+                  }
+                };
+                req.session.customer.loggedIn = true;
+                res.json(req.session.customer);
+              }
             }
-          } else {
-            console.log("invalid email or password");
-          }
-        });
-      });
+            //if password doesn't match
+            else {
+              console.log("invalid password");
+              res.json("invalid");
+            }
+        })
+      }
+    })     
   },
   session: (req, res) => {
     res.json(req.session.customer);
