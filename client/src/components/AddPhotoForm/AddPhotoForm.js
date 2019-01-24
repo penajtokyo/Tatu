ea47d62c48fbc781cbc79fb5bc257d64bd737ef9
Andrea Carlstrom <andi.carlstrom@gmail.com>
2019-01-24 10:20:49 -0700

    working on error modal

--- client/src/components/AddPhotoForm/AddPhotoForm.js
+++ client/src/components/AddPhotoForm/AddPhotoForm.js
@@ -3,132 +3,172 @@ import { Button, Col, Row, Modal, Input } from "react-materialize";
 import SelectPlacement from "../SelectPlacement";
 import SelectStyle from "../SelectStyle";
 import API from "../../utils/API";
+import MissingValModal from "../Modals/MissingVal";
 
 class AddPhotoForm extends Component {
- state = {
-   url: "",
-   style: "",
-   placement: "",
-   description: ""
- };
-
- // Method that sets the photo data that is being shipped to the DB
- handleAddPhoto = () => {
-   const photoData = {
-     file: this.state.url,
-     style: this.state.style,
-     placement: this.state.placement,
-     description: this.state.description
-   };
-
-   // Call to utils.API.addImage post request to the backend
-   API.addImage(photoData)
-     .then(res => {
-       this.setState({
-         url: "",
-         style: "",
-         placement: "",
-         description: ""
-       });
-     })
-     .catch(err => console.log(err));
- };
-
- // Submit form event handler that check's to make sure that the user has provided all of the necessary information in the form
- handleSubmit = event => {
-   event.preventDefault();
-   console.log("Working?");
-
-   if (
-     this.state.url &&
-     this.state.style &&
-     this.state.placement &&
-     this.state.description
-   ) {
-     this.handleAddPhoto();
-     window.location.reload();
-   } else {
-     alert("Oops! It looks like something's missing.");
-   }
- };
-
- // Event handler to set the form data to corresponding state key's
- handleInputChange = event => {
-   const name = event.target.name;
-   const value = event.target.value;
-
-   this.setState({
-     [name]: value
-   });
- };
-
- // Method for removing content from modals
- resetModal = () => {
-   console.log("hi");
-   this.setState({
+  state = {
     url: "",
     style: "",
     placement: "",
-    description: ""
-   })
- }
+    description: "",
+    // Setting the default state === to false
+    missingVal: false
+  };
 
- render() {
-   return (
-     <div>
-       <Row>
-         <Col s={12} m={12} l={12} className="center artist-container" id={this.props}>
-           {/* Modal button that displays on the Artist Profile page */}
-           <Modal
-             header="Add a photo"
-             trigger={
-              <Button faicon="fa fa-plus" floating icon="add_a_photo" className="photo-btn" />
-             }
-             actions={
-              <div>
-                <Button className="cancel-btn" onClick={this.resetModal} flat modal="close" waves="light">Cancel</Button>
-                <Button waves="light" type="submit" value="Submit" className="modal-action modal-close" onClick={this.handleSubmit}>UPDATE</Button>
-              </div>}
-           >
-             <Row>
-               {/* Add photo form */}
-               <form onSubmit={this.handleSubmit}>
-                 <Input
-                   s={12}
-                   type="url"
-                   name="url"
-                   label="Image URL"
-                   onChange={this.handleInputChange}
-                 />
+  // Method that sets the photo data that is being shipped to the DB
+  handleAddPhoto = () => {
+    const photoData = {
+      file: this.state.url,
+      style: this.state.style,
+      placement: this.state.placement,
+      description: this.state.description
+    };
 
-                 <SelectPlacement
-                   s={12}
-                   handleSelection={this.handleInputChange}
-                 />
+    // Call to utils.API.addImage post request to the backend
+    API.addImage(photoData)
+      .then(res => {
+        this.setState({
+          url: "",
+          style: "",
+          placement: "",
+          description: ""
+        });
+      })
+      .catch(err => console.log(err));
+  };
 
-                 <SelectStyle
-                   s={12}
-                   handleSelection={this.handleInputChange}
-                 />
+  // Submit form event handler that check's to make sure that the user has provided all of the necessary information in the form
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("Working?");
 
-                 <Input
-                   s={12}
-                   type="text"
-                   name="description"
-                   label="Description"
-                   onChange={this.handleInputChange}
-                 />
+    if (
+      this.state.url &&
+      this.state.style &&
+      this.state.placement &&
+      this.state.description
+    ) {
+      this.handleAddPhoto();
+      window.location.reload();
+    } else {
+      // alert("Oops! It looks like something's missing.");
+      this.handleMissingVal();
+    }
+  };
 
-                 {/* Submit button to add photo */}
-                 
-               </form>
-             </Row>
-           </Modal>
-         </Col>
-       </Row>
-     </div>
-   );
- }
+  handleMissingVal = () => {
+    //   // this.errModal();
+    this.setState({
+      missingVal: true
+    });
+  };
+
+  // Event handler to set the form data to corresponding state key's
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // Method for removing content from modals
+  resetModal = () => {
+    console.log("hi");
+    this.setState({
+      url: "",
+      style: "",
+      placement: "",
+      description: ""
+    });
+  };
+
+  render() {
+    console.log(this.state.missingVal);
+    // {this.state.missingVal ? this.}
+    return (
+      <div>
+        <Row>
+          <Col
+            s={12}
+            m={12}
+            l={12}
+            className="center artist-container"
+            id={this.props}
+          >
+            <MissingValModal missingValModal={this.state.missingVal} />
+            <Modal
+              header="Add a photo"
+              trigger={
+                <Button
+                  faicon="fa fa-plus"
+                  floating
+                  icon="add_a_photo"
+                  className="photo-btn"
+                />
+              }
+              actions={
+                <div>
+                  <Button
+                    className="cancel-btn"
+                    onClick={this.resetModal}
+                    flat
+                    modal="close"
+                    waves="light"
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    waves="light"
+                    type="submit"
+                    value="Submit"
+                    className="modal-action modal-close"
+                    onClick={this.handleSubmit}
+                  >
+                    UPDATE
+                  </Button>
+                </div>
+              }
+            >
+              <Row>
+                {/* Add photo form */}
+                <form onSubmit={this.handleSubmit}>
+                  <Input
+                    s={12}
+                    type="url"
+                    name="url"
+                    label="Image URL"
+                    onChange={this.handleInputChange}
+                  />
+
+                  <SelectPlacement
+                    s={12}
+                    handleSelection={this.handleInputChange}
+                  />
+
+                  <SelectStyle
+                    s={12}
+                    handleSelection={this.handleInputChange}
+                  />
+
+                  <Input
+                    s={12}
+                    type="text"
+                    name="description"
+                    label="Description"
+                    onChange={this.handleInputChange}
+                  />
+
+                  {/* Submit button to add photo */}
+                </form>
+              </Row>
+            </Modal>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default AddPhotoForm;
---
+++ client/src/components/Modals/MissingVal.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Modal, Button, Col, Row } from "react-materialize";
+
+class MissingVal extends Component {
+  render() {
+    console.log(this.props.missingValModal);
+    // if (this.props.missingValModal) {
+    return (
+      <div>
+        <Modal header="Modal Header" trigger={<Button>Use Me!</Button>} />
+      </div>
+      //   <div className="modal">
+      //     <div className="modal-content">
+      //       <Row>
+      //         <Col s={12} m={12}>
+      //           <h4 className="modalHeader">Oops!</h4>
+      //         </Col>
+      //         <Col s={12} m={12}>
+      //           <p>Oops! It looks like something's missing</p>
+      //           {/* <Verification addressVerified={props.addressVerified} /> */}
+      //         </Col>
+      //       </Row>
+      //     </div>
+      //     <div className="modal-foot" />
+      //   </div>
+    );
+    // } else {
+    //   console.log("nope not working!");
+    // }
+  }
+}
+
+export default MissingVal;
+
+// trigger = {};
